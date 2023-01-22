09a05dff06c8f630b9c4261e87c2f17bebc83f94
DawidKahla <dawid.kahla@onet.pl>
2023-01-22 16:59:22 +0100

    Add print shopping_dict

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ shopping_dict = {
     "piekarnia" : ['chleb', 'pączek', 'rukola'],
     "warzywniak" : ['marchew', 'seler', 'rukola']
 }
+X = 0
+print('Lista zakupów')
+for place in shopping_dict:
+    print(f"Idę do {place.title()}, kupuję tu następujące rzeczy: {', '.join([product.title() for product in shopping_dict[place]])}.")
+    X += len(shopping_dict[place])
+print(f"W sumię kupuję {X} produktów.")
